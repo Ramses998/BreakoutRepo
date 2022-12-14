55432fc6a6205d77e827bee862b11d58a218530b
Ramses Figueroa <ramsesfig98@live.com>
2022-12-14 11:26:27 -0800

    Avance módulo 1
    Avance módulo 13

---
+++ Breakout/Assets/_Scripts/MuestraEventos.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MuestraEventos : MonoBehaviour
+{
+    public UnityEvent MiEventoUnity;
+    public event EventHandler EnCasoDeEspacioPresionado;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        EnCasoDeEspacioPresionado += EventoEscuchado;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            EnCasoDeEspacioPresionado?.Invoke(this, EventArgs.Empty);
+            MiEventoUnity.Invoke();
+        }
+    }
+
+    public void EventoEscuchado(object sender, EventArgs e) {
+        Debug.Log("El evento se escuchó correctamente");
+    }
+
+    public void EventoUnityDisparado() {
+        Debug.Log("El evento Unity se disparó correctamente");
+    }
+}
